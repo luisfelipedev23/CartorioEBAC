4fc44525690bba99c4189136c8a2e3d521ab867a
luisfelipedev23 <lfmartinsbarbosa@gmail.com>
2025-05-21 10:39:54 -0300

    Opção de saída

--- Cartório.cpp
+++ Cartório.cpp
@@ -138,6 +138,7 @@ int main()
 	    printf("\t1 - Registrar nomes\n");
 	    printf("\t2 - Consultar nomes\n");
 	    printf("\t3 - Deletar nomes\n\n"); 
+	    printf("\t4 - Sair do sistema\n\n");
 	    printf("Opção: "); //fim do menu
 	
 	    scanf("%d" , &opcao); //armazenando a escolha do usuário 
@@ -158,6 +159,11 @@ int main()
 			case 3:
 		    deletar();
     	    break;    	
+    	    
+    	    case 4:
+    	    printf("Oberigador por utilizar o sistema!\n");
+    	    return 0;
+    	    break;
     	    
     	    default:
     	    printf("Essa opção não está disponível!\n");
